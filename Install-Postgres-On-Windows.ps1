1f70dcf4533d9c1634123ebcc3179c259892b195
Darian Miller <darian@darianmiller.com>
2024-02-03 16:12:57 -0800

    closes #1  Initial release

---
+++ Install-Postgres-On-Windows.ps1
@@ -0,0 +1,285 @@
+<#
+.Synopsis
+   Install PostgreSQL on Windows from a binaries zipfile as published by EDB  
+
+.EXAMPLE
+   Extract the latest release zip to 'C:\Postgres' and create/start a 'Postgres' named Windows service:
+   Install-Postgres-On-Windows.ps1 -ArchiveFileName postgresql-16.1-1-windows-x64-binaries.zip
+
+.EXAMPLE
+   Download latest and install to 'C:\Postgres' and create/start a 'Postgres' named Windows service:
+   Install-Postgres-On-Windows.ps1 -DownloadLatest
+
+.EXAMPLE
+   Extract the release zip named 'Latest.zip' to 'C:\Server\PostgreSQL' and create/start a 'PostgreSQL' named Windows service with the server listening on port 5433
+   Add C:\Server\PostgreSQL\bin to the system PATH environment variable
+   Also extracts pgAdmin 4 into the 'C:\Server\PostgreSQL\Admin' folder
+   Install-Postgres-On-Windows.ps1 -ArchiveFileName Latest.zip -DestinationPath C:\Server\PostgreSQL -ServiceName PostgreSQL -ListenPort 5433 -UpdatePath -InstallPGAdmin
+
+.NOTES
+   Latest version is on GitHub: https://github.com/darianmiller/install-postgres-on-windows 
+   Version 1.0 2024-02-03 Darian Miller
+   MIT Licensed
+
+.Parameter ArchiveFileName
+    Specify full path of PostgreSQL binaries release zip filename (or use the DownloadLatest switch)
+
+.Parameter DestinationPath
+    Specify path to destination folder, defaults to 'C:\Postgres'
+
+.Parameter ServiceName
+    Specify the name of the Windows Service, defaults to 'Postgres'
+    
+.Parameter ListenPort
+    Provide optional integer value to override the default port of 5432
+
+.Parameter DownloadLatest
+    Optional switch to download latest version from GitHub (can be used instead of specifying archive file)
+
+.Parameter UpdatePath
+    Optional switch to add the Postgres\bin folder to the PATH
+
+.Parameter InstallPGAdmin
+    Optional switch to also extract pgAdmin
+#>
+
+param(
+    [string]$ArchiveFileName,
+    [string]$DestinationPath = "C:\Postgres",
+    [string]$ServiceName = "Postgres",
+    [int]$ListenPort = 5432,
+    [switch]$DownloadLatest,
+    [switch]$UpdatePath,
+    [switch]$InstallPGAdmin
+)
+
+
+#Latest binary zipfiles are listed first, starting with Windows
+#As of Feb 03, 2024, latest version is: "postgresql-16.1-1-windows-x64-binaries.zip" (331MB)
+#which correlates to:  https://sbp.enterprisedb.com/getfile.jsp?fileid=1258791
+function Get-FirstMatchingDownloadHref {
+    $releasesUri = "https://www.enterprisedb.com/download-postgresql-binaries"
+
+    Write-Verbose "Getting list of release binary packages available from '$releasesUri'"
+
+    $htmlContent = Invoke-WebRequest -Uri $releasesUri | Select-Object -ExpandProperty Content
+
+    $regexPattern = 'href=["''](https://sbp.enterprisedb.com/getfile.jsp\?fileid=[^"'']*)["'']'
+    $matches = [regex]::Matches($htmlContent, $regexPattern)
+
+    if ($matches.Count -gt 0) {
+        return $matches[0].Groups[1].Value
+    }
+    else {
+        return $null
+    }
+}
+
+
+function DownloadLatestReleaseFromEDB {
+
+    $Result = ""
+  
+    Write-Host "- Attempting to download latest binary zip"
+    $downloadUri = Get-FirstMatchingDownloadHref($releasesUri)
+  
+    if ($downloadUri -ne $null) {
+  
+        Write-Verbose "- Latest Binaries zip from EDB determined to be: '$downloadUri'"
+        Write-Host "- Downloading release zip"
+  
+        $Result = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath "Postgres-Windows-Binaries.zip"
+  
+        Write-Verbose "Temporary file to be created: '$Result'"
+  
+        Invoke-WebRequest -Uri $downloadUri -Out $Result        
+    }
+    else {
+  
+        Throw "ERROR: Could not determine latest release' (Temporary EDB website issues or an update of this script is required)"
+  
+    }
+  
+    return $Result
+}
+
+function Unzip {
+    param (
+        [string]$ArchiveFileName,
+        [string]$DestinationPath
+    )
+  
+    Write-Host "- Extracting archive file '$ArchiveFileName' to '$DestinationPath'"
+  
+    #Not using powershell command as it does not offer --strip-components type functionality
+    tar -xf "$ArchiveFileName" --strip-components=1 --exclude="./doc/*" --exclude="./include/*" --exclude="./StackBuilder/*" --exclude="./symbols/*" --exclude="./pgAdmin 4/*" -C "$DestinationPath\Server"
+    if ($InstallPGAdmin) {
+        tar -xf "$ArchiveFileName" --strip-components=2 -C "$DestinationPath\Admin" "pgsql/pgAdmin 4/"
+    }
+      
+    if ($LASTEXITCODE -ne 0) {
+  
+        throw "ERROR: Failed to extract '$ArchiveFileName', TAR exit code '$LASTEXITCODE'"
+  
+    } 
+}
+
+function SetDefaultPort {
+    param (
+        [int] $ListenPort,
+        [string] $DataDirectory 
+    )
+    $confFilePath = Join-Path $DataDirectory "postgresql.conf"
+
+    if (Test-Path $confFilePath -PathType Leaf) {
+
+        $confContent = Get-Content -Path $confFilePath 
+        $portUpdated = $false
+
+        for ($i = 0; $i -lt $confContent.Count; $i++) {
+            $line = $confContent[$i]
+
+            if ($line -match '^\s*#?\s*port\s*=\s*\d+') {
+
+                $confContent[$i] = "port = $ListenPort"
+                $portUpdated = $true
+                Write-Host "Port number updated to $ListenPort"
+                break 
+            }
+        }
+
+        if ($portUpdated) {
+            $confContent | Set-Content $confFilePath
+            Write-Host "Saved changes to postgresql.conf"
+        }
+        else {
+            Write-Host "Error: Port setting not found in postgresql.conf"
+        }   
+    }
+    else {
+        Write-Error "Error: postgresql.conf file not found in $DataDirectory"
+    }
+}  
+
+  
+$PG_SUPERUSER = "postgres"
+$PG_SERVERHOME = "$DestinationPath\Server"
+$PG_ADMINHOME = "$DestinationPath\Admin"
+$PG_DATAHOME = "$DestinationPath\Data"
+
+
+if (Test-Path $DestinationPath) {
+    #Prevent accidental corruption of an existing instance
+    Write-Host "PostgreSQL root folder already exists [$DestinationPath]"
+    exit 101
+}
+
+Write-Verbose "- Creating installation directories within '$PG_SERVERHOME'"
+New-Item -ItemType Directory -Path $PG_SERVERHOME | Out-Null
+if ($InstallPGAdmin) {
+    New-Item -ItemType Directory -Path $PG_ADMINHOME | Out-Null
+}
+#Note: do not auto-create the DATA folder - let InitDB create it so it can set proper permissions
+#New-Item -ItemType Directory -Path $PG_DATAHOME | Out-Null
+
+
+if ($DownloadLatest) {
+    $tempFile = DownloadLatestReleaseFromEDB
+    if ($tempFile -ne "") {
+        try {
+            Unzip -ArchiveFileName $tempFile -DestinationPath $DestinationPath
+        }
+        finally {
+            if (Test-Path -Path $tempFile -PathType Leaf) {
+  
+                Write-Verbose "Removing Temporary downloaded file: '$tempFile'"
+                Remove-Item $tempFile -Force
+            }
+        }
+    }
+}
+else {    
+    if ($ArchiveFileName -ne "") {
+        Unzip -ArchiveFileName $ArchiveFileName -DestinationPath $DestinationPath
+    }
+    else {
+
+        Throw "ArchiveName (or DownloadLatest switch) not specified"
+
+    }
+}
+
+
+if ($UpdatePath) {
+    $currentEnv = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+    Write-Verbose "Current PATH: '$currentEnv'"
+    if ($currentEnv -notlike "*$PG_SERVERHOME\bin*") {
+        $newPath = "$currentEnv;$PG_SERVERHOME\bin"
+
+        Write-Host "- Adding Postgres bin folder to system PATH: '$PG_SERVERHOME\bin'"
+        Write-Verbose "New system PATH will be: '$newPath'"
+
+        [Environment]::SetEnvironmentVariable("PATH", $newPath, [System.EnvironmentVariableTarget]::Machine)
+    }
+    else {
+
+        Write-Verbose "Note: System PATH already contains '$PG_SERVERHOME\bin'"
+
+    }
+    $env:PATH += ";$PG_SERVERHOME\bin"
+}
+
+
+Write-Host "- Initializing a new Postgres database cluster using initdb"
+Write-Host
+
+# This command sets up the necessary directory structure and initializes the configuration files (like address/port/mem allocation)
+# Databases within this cluster (root folder) share the same PostgreSQL server process (instance)
+$env:TZ = "UTC"
+
+#Workaround for Windows ownership issues - not needed if the data folder doesn't exist yet
+#icacls "$PG_DATAHOME" /grant Everyone:F /T
+
+Add-Content -Path temp_pgsql.pw -Value "postgres"
+try {
+    & "$PG_SERVERHOME\bin\initdb.exe" -D "$PG_DATAHOME" -E UTF8 -U "$PG_SUPERUSER" --locale=en_US.UTF-8 --pwfile=temp_pgsql.pw --no-instructions    
+}
+finally {
+    Remove-Item -Path temp_pgsql.pw -Force
+}
+
+if ($ListenPort -ne 5432) {
+    SetDefaultPort -ListenPort $ListenPort -DataDirectory $PG_DATAHOME
+}
+
+
+Write-Host
+Write-Host "- Adding and starting a new Windows Service named: '$ServiceName'"
+Write-Host
+
+& "$PG_SERVERHOME\bin\pg_ctl.exe" register -N "$ServiceName" -U LocalSystem -D "$PG_DATAHOME"
+
+#Note: PowerShell 7 is required for DelayedAuto option
+#Set-Service -Name "PostgreSQL" -StartupType DelayedAuto
+& "sc.exe" config "$ServiceName" start=delayed-auto
+
+#All done - start up Postgres!
+& "net" start "$ServiceName"
+$serviceState = (Get-Service -Name $ServiceName).Status
+if ($serviceState -ne "Running") {
+    Throw "Failed to start $ServiceName service."
+}
+
+#toconsider: optionally set environment variables:
+# PGHOST, PGPORT, PGPASSWORD, PGDATABASE, PGOPTIONS...
+#[Environment]::SetEnvironmentVariable("PGDATA", "$PG_DATAHOME", [EnvironmentVariableTarget]::Machine)
+#[Environment]::SetEnvironmentVariable("PGUSER", "$PG_SUPERUSER", [EnvironmentVariableTarget]::Machine)
+
+#toconsider: Display version and exit, just for logging
+#& "$PG_SERVERHOME\bin\initdb.exe" --version
+
+#toconsider: List all available databases then exit, just for logging
+# Note, will use current Windows username by default if -U xxx or --username=xxx not specified
+# & "$PG_SERVERHOME\bin\psql" -p $ListenPort -U "$PG_SUPERUSER" -l
+
+Write-Host "PostgreSQL installed!" -ForegroundColor Green
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # install-postgres-on-windows
-Simple PowerShell script to install PostgreSQL from a binary release zip on Windows
+PowerShell script to install [PostgreSQL](https://postgresql.org/) from a binary release zip on Windows.
+
+- No dependencies
+- MIT Licensed
+- Download binary release zips from EDB: https://www.enterprisedb.com/download-postgresql-binaries
+- Associated blog article can be found here: https://www.ideasawakened.com/
+
+# Summary
+You can simply run the script with the DownloadLatest switch (`Install-Postgres-On-Windows.ps1 -DownloadLatest`) and the latest release zip will be downloaded, then automatically installed to `C:\Postgres`, and you will end up with a new Windows Service named `Postgres` actively running and accepting all local connections on port 5432.  Alternately, manually download the latest release zip and provide it via the ArchiveFileName parameter. (`Install-Postgres-On-Windows.ps1 -ArchiveFileName postgresql-16.1-1-windows-x64-binaries.zip`)
+
+
+# Parameters
+
+Note: you need to provide at least one parameter as you must either specify the `ArchiveFileName` or the `DownloadLatest` switch.
+
+## ArchiveFileName
+This string parameter is the release zip to install (as previously downloaded from EDB.)  An example of the current latest release zip is `postgresql-16.1-1-windows-x64-binaries.zip` which is 330MB in size.
+
+## DestinationPath
+This optional string parameter is the path to unzip the binaries into and it defaults to `C:\Postgres` if not provided. In a full installation, there will be three child folders created: Admin, Data, and Server
+
+## ServiceName
+This optional string parameter is the name of the new Windows Service created.  If not provided, the ServiceName defaults to `Postgres`
+
+
+## ListenPort
+If specified, this optional integer parameter will override the default TCP port the server listens on which is [5432 by default](https://www.postgresql.org/docs/current/runtime-config-connection.html) as set in `postgresql.conf`
+
+## DownloadLatest
+If specified, this optional switch parameter overrides the ArchiveFileName and the latest release zip will be downloaded from EDB's website and installed.  Unfortunately there does not seem to be an API available for this, so the script currently resorts to web scraping (and may be prone to breakage if they alter the format of their site.)  The script will parse the files found on the [download-postgresql-binaries](https://www.enterprisedb.com/download-postgresql-binaries) page and pick the first one (as it will be the latest Windows installer.)  The file will be saved as `Postgres-Windows-Binaries.zip` within the TEMP folder and deleted once the installation is completed.
+
+## UpdatePath
+If this optional switch is specified, the script will update the PATH with the Postgres\bin folder (useful if you are running command line tools like psql.exe.)
+
+## InstallPGAdmin
+If this optional switch is specified, the script will create an Admin child folder and unzip the [pgAdmin 4](https://www.pgadmin.org/download/pgadmin-4-windows/) files.  Including this switch adds about 760MB worth of space and increases the installation time.  This is useful if you do not have pgAdmin or another tool like [HeidiSQL](https://www.heidisql.com/) already installed.
+
+# Custom Settings
+
+I have added a few common parameters to configure the instance, and more may be added in the future.  But, for now, these following settings were used for [initdb](https://www.postgresql.org/docs/current/app-initdb.html) during the initial installation process:
+
+- The default [encoding](https://www.postgresql.org/docs/current/multibyte.html#MULTIBYTE-CHARSET-SUPPORTED) is set to `UTF8` with a [locale](https://www.postgresql.org/docs/current/locale.html) of `en_US.UTF-8`
+- The folder used for the [database cluster](https://www.postgresql.org/docs/current/glossary.html#GLOSSARY-DB-CLUSTER) is set to `DestinationPath`\Data
+- The new Windows Service created is set to `Automatic (Delayed Startup)` and the service is started during the installation process.
+- The [bootstrap superuser](https://www.postgresql.org/docs/current/glossary.html#GLOSSARY-BOOTSTRAP-SUPERUSER) is named `postgres` and has `postgres` as its password
+- The default [time zone](https://www.postgresql.org/docs/current/datatype-datetime.html#DATATYPE-TIMEZONES) is set to `UTC`
+
+All settings not set by parameters, or listed above in Custom Settings, will utilize the Postgres default values, such as:
+- The default [authentication method](https://www.postgresql.org/docs/current/auth-methods.html) is `trust` which is meant for local workstations, not multi-user servers.
+- The default [max_connections](https://www.postgresql.org/docs/current/runtime-config-connection.html#GUC-MAX-CONNECTIONS) is set automatically by initdb and is typically set to `100`.
+
